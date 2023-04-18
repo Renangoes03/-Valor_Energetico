a77165253508f3b3a1ab0ac7ac95f12881421849
Renangoes03 <renangoessantos9@gmail.com>
2023-04-18 10:32:35 -0300

    First

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GastoComponent } from './gasto/gasto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GastoComponent
   ],
   imports: [
     BrowserModule,
